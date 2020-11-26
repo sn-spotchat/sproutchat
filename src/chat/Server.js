c153cea486c7e1f1ea47654c4be34aeb9385470d
Sejin Kim <lemon05090@gmail.com>
2020-11-27 00:10:05 +0900

    try to chat

--- src/chat/Server.js
+++ src/chat/Server.js
@@ -18,8 +18,10 @@ server.listen(port, () => {
     console.log(`server open ${port}`);//이건 출력 된당
 }); 
 
+var onlineUsers = {};
 
-io.on('connection', function (socket) {
+
+io.sockets.on('connection', function (socket) {
     console.log('server connection')
 
     socket.on('login' , (data, cb) => {
@@ -42,4 +44,77 @@ io.on('connection', function (socket) {
     socket.on('room', (data) => {
         socket.emit('room', data);
     })
+    socket.on('new message', function (data) {
+        socket.emit('new message', data);
+    })
+
+    socket.on('join room', function (data) {
+        let id = getUserBySocketId(socket.id);
+        let prevRoomId = onlineUsers[id].roomId;
+        let nextRoomId = data.roomId;
+        socket.leave('room' + prevRoomId);
+        socket.join('room' + nextRoomId);
+        onlineUsers[id].roomId = data.roomId;
+        updateUserList(prevRoomId, nextRoomId, id);
+    });
+    
+    socket.on('logout', function () {
+        if (!socket.id) return;
+        let id = getUserBySocketId(socket.id);
+        let roomId = onlineUsers[id].roomId;
+        delete onlineUsers[getUserBySocketId(socket.id)];
+        updateUserList(roomId, 0, id);
+    });
+
+    socket.on("send message", function (data) {
+        io.sockets.io('room' + data.roomId).emit('new message', {
+            name: getUserBySocketId(socket.id),
+            socketId: socket.id,
+            msg: data.msg
+        });
+    });
+  
+    function updateUserList(prev, next, id) {
+        if (prev !== 0) {
+            io.sockets.in('room' + prev).emit("userlist", getUsersByRoomId(prev));
+            io.sockets.in('room' + prev).emit("lefted room", id);
+            console.log("prev"+ prev);
+        }
+        if (next !== 0) {
+            io.sockets.in('room' + next).emit("userlist", getUsersByRoomId(next));
+            io.sockets.in('room' + next).emit("joined room", id);
+            console.log("next"+ next);
+        }
+    }
+
+
+    function getUserBySocketId(id) {
+        return Object.keys(onlineUsers).find(key => onlineUsers[key].socketId === id);
+    }
+
+    function updateUserList(prev, next, id) {
+        if (prev !== 0) {
+            io.sockets.in('room' + prev).emit("userlist", getUsersByRoomId(prev));
+            io.sockets.in('room' + prev).emit("lefted room", id);
+            console.log("prev"+ prev);
+        }
+        if (next !== 0) {
+            io.sockets.in('room' + next).emit("userlist", getUsersByRoomId(next));
+            io.sockets.in('room' + next).emit("joined room", id);
+            console.log("next"+ next);
+        }
+    }
+
+    function getUsersByRoomId(roomId) {
+        let userstemp = [];
+        Object.keys(onlineUsers).forEach((el) => {
+            if (onlineUsers[el].roomId === roomId) {
+                userstemp.push({
+                    socketId: onlineUsers[el].socketId,
+                    name: el
+                });
+            }
+        });
+        return userstemp;
+    }
 });
